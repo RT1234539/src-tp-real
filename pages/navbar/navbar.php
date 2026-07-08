c7c11bf89287754154690c1936e56dd8df9721bd
RT233656989 <llgamlnglllegamer@gmail.com>
2026-07-08 09:56:42 +0300

    integration : search

---
+++ pages/navbar/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <ul>
+        <li class="brand">Employés DB</li>
+        <li><a href="emp_form.php">Ajouter un employé</a></li>
+        <li><a href="dept_form.php">Ajouter un département</a></li>
+        <li><a href="search.php">Rechercher</a></li>
+        <li><a href="stats.php">Statistiques</a></li>
+    </ul>
+</nav>
